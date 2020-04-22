68e2c0eb6b9ba7a0b12e04805fdb3f6cf87f2e1f
YanaBrewster <yana.brewster@gmail.com>
2020-04-22 12:42:22 +1200

    Modal added

--- src/components/productcard/ProductCard.js
+++ src/components/productcard/ProductCard.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from '../modal/Modal';
+
+
 
 class ProductCard extends React.Component{
+
+  modal = ()=>{
+    const modalContent=(
+      <Modal title={this.props.tit} description={this.props.des} img={this.props.imgurl}/>
+    )
+    ReactDOM.render(modalContent,document.getElementById('root'));
+
+  }
+
   render(){
     return(
-      <div className="card col-3 col-lg-3 text-info my-2 mx-2>
+      <div className="card col-6 text-info my-5" style={{width: '18rem'}}>
         <img src={this.props.imgurl} className="card-img-top" alt="Wine" />
         <div className="card-body">
           <h5 className="card-title">{this.props.tit}</h5>
           <p className="card-text">{this.props.des} </p>
-          <button className="btn btn-primary">View more</button>
+          <button className="btn btn-primary" onClick={this.modal}>View more</button>
         </div>
       </div>
-
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Nav from './components/nav/Nav';
-import Products from './components/products/Products';
 
 
 ReactDOM.render(
-  <div className="">
-    <div className="header">
-      <h1 className="h1"> React Demo Site</h1>
-      <div className="buttons">
-      <Nav/>
-      </div>
-    </div>
-    <Home/>
-    <About/>
-    <Products/>
-  </div>,
+  <App/>,
   document.getElementById('root'));
